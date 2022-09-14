a76e27f401c5d0000a21f3ee1dfd9d88ec3b1037
Matěj Pešl <matejpesl1@gmail.com>
2022-09-14 12:25:25 +0200

    finish cards template

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+enum ActionButtonType { voiceNote, textNote }
+
 void main() {
-  //_notesList.add(Note(DateTime.now(), textContent: "some text"));
   runApp(const MyApp());
 }
 
@@ -54,22 +55,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-/*class Notes extends StatefulWidget {
-  const Notes({Key? key}) : super(key: key);
-
-  @override
-  State<Notes> createState() => _NotesState();
-}
-
-class _NotesState extends State<Notes> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}*/
-
 class NoteList extends StatefulWidget {
   const NoteList({Key? key}) : super(key: key);
 
@@ -82,26 +67,34 @@ class _NoteListState extends State<NoteList> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
+    _notes.add(Note(DateTime.now(), textContent: "some text")); // TODO: Remove this debug.
+    Widget body;
+    if (_notes.isEmpty) {
+      body = const Center(
+          child: Text(
+              "You have no ongoing reminders!",
+              textAlign: TextAlign.center
+          )
+      );
+    } else {
+      body = ListView.builder(
         itemCount: _notes.length,
         itemBuilder: (context, i) {
-          if (_notes.isEmpty) {
-            return const Text("no note");
-          }
-
           return _notes[i];
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _notes.add(Note(DateTime.now(), textContent: "some text"));
-          });
-        },
-        backgroundColor: Colors.green,
-        child: const Icon(Icons.navigation),
-      ),
+      );
+    }
+    return Scaffold(
+      body: body,
+      floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const ActionButton(ActionButtonType.voiceNote),
+            ActionButton(ActionButtonType.textNote, onTouchDown: () {
+              setState(() => _notes.add(Note(DateTime.now(), textContent: "some text")));
+            })
+          ]
+      )
     );
   }
 }
@@ -124,96 +117,69 @@ class Note extends StatefulWidget {
 }
 
 class _NoteState extends State<Note> {
-
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        widget.textContent!,
-      ),
-      trailing: const Icon(
-        Icons.favorite,
-        color: Colors.red,
-        semanticLabel: 'Save',
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              "zadáno 1.9. 16:00",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  color: Colors.grey
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              widget.textContent.toString(),
+              textAlign: TextAlign.left,
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                const Text(
+                  "Proběhne za 30 minut",
+                  style: TextStyle(
+                    color: Colors.grey
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                TextButton(
+                  child: const Text('OZNAČIT ZA DOKONČENÉ'),
+                  onPressed: () {/* ... */},
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-/*class _NotesState extends State<Notes> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
+class ActionButton extends StatelessWidget {
+  final ActionButtonType type;
+  final Function? onTouchDown;
+  final Function? onTouchUp;
+  final Function? onPress;
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      // Add lines from here...
-      // The route / the screen
-      MaterialPageRoute<void>(
-        // The content of the route/screen.
-        builder: (context) {
-          final tiles = _saved.map(
-                (pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ), // ...to here.
-    );
-  }
+  const ActionButton(this.type, {super.key, this.onTouchDown, this.onTouchUp, this.onPress });
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: *//*1*//* (context, i) {
-        if (i.isOdd) return const Divider(); *//*2*//*
-
-        final index = i ~/ 2; *//*3*//*
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); *//*4*//*
-        }
-
-        final alreadySaved = _saved.contains(_suggestions[index]);
-        return ListTile(
-          title: Text(
-            _suggestions[index].asString,
-            style: _biggerFont,
-          ),
-          trailing: Icon(
-            alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
-            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-          ),
-          onTap: () {
-            setState(() {
-              if (alreadySaved) {
-                _saved.remove(_suggestions[index]);
-              } else {
-                _saved.add(_suggestions[index]);
-              }
-            });
-          },
-        );
-      },
+    return GestureDetector(
+      onTapDown: (_) {onTouchDown?.call();},
+      onTapUp: (_) {onTouchUp?.call();},
+      child: FloatingActionButton(
+        onPressed: () {onPress?.call();},
+        heroTag: type.toString(),
+        child: Icon(type == ActionButtonType.voiceNote ? Icons.mic : Icons.short_text_outlined),
+      ),
     );
   }
-}*/
+}

392b595477200869ca110206d6a39aca75dec166
Matěj Pešl <matejpesl1@gmail.com>
2022-09-16 11:35:27 +0200

    finish barebone card logic

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:intl/intl.dart';
@@ -68,7 +71,7 @@ class _NoteListState extends State<NoteList> {
 
   @override
   Widget build(BuildContext context) {
-    _notes.add(Note(DateTime.now(), textContent: "This is some note that you can note.")); // TODO: Remove this debug.
+    _notes.add(Note(DateTime.now().add(const Duration(minutes: 26)), textContent: "This is some note that you can note.")); // TODO: Remove this debug.
     Widget body;
     if (_notes.isEmpty) {
       body = const Center(
@@ -117,63 +120,108 @@ class Note extends StatefulWidget {
   State<Note> createState() => _NoteState();
 }
 
+enum UnitType { minutes, hours }
 class _NoteState extends State<Note> {
+  DateTime? timeMarkedDone = null;
 
-  String formatDifference(Duration difference) {
-    final bool inMinutes = difference.inMinutes < 60;
-    final value = inMinutes ? difference.inHours : difference.inMinutes;
+  String getUnitFormatted(int value, UnitType type) {
+    bool inMinutes = type == UnitType.minutes;
 
     final unitFormatFive = inMinutes ? "minut" : "hodin";
     final unitFormatTwo = inMinutes ? "minuty" : "hodiny";
-    final unitFormatOne = inMinutes ? "minuta" : "hodina";
-    final unit = value >= 5 ? unitFormatFive : value >= 2 ? unitFormatTwo : unitFormatOne;
-    return "$value $unit";
+    final unitFormatOne = inMinutes ? "minutu" : "hodinu";
+    return value >= 5 ? unitFormatFive : value >= 2 ? unitFormatTwo : unitFormatOne;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(minutes: 1), (Timer t) => setState((){}));
+  }
+
+  String formatDifference(Duration difference) {
+    final minutes = difference.inMinutes;
+    final hours = difference.inHours;
+
+    final String minutesFormatted = minutes == 0 ? "" : "$minutes ${getUnitFormatted(minutes, UnitType.minutes)}";
+    String hoursFormatted = hours == 0 ? "" : "$hours ${getUnitFormatted(hours, UnitType.hours)}";
+
+    if (minutesFormatted != "" && hoursFormatted != "")
+      hoursFormatted += " a ";
+
+    return "$hoursFormatted $minutesFormatted";
   }
 
   @override
   Widget build(BuildContext context) {
-    final difference = widget.creationTime.difference(widget.dueTime);
-    final differenceFormatted = formatDifference(difference);
-    final creationTimeFormatted = DateFormat("d.M. H:m").format(widget.creationTime);
-    final dueTimeFormatted = "Proběhne za $differenceFormatted (v ${DateFormat("H:m").format(widget.dueTime)})";
+    final dueTime = timeMarkedDone ?? widget.dueTime;
+    final difference = dueTime.difference(DateTime.now());
+    final isTomorrow = DateTime.now().day < dueTime.day;
 
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "zadáno $creationTimeFormatted",
-              textAlign: TextAlign.left,
-              style: const TextStyle(
-                  color: Colors.grey
-              ),
+    final differenceInWords = formatDifference(difference);
+    String dueTimeInWords;
+    if (difference.isNegative) {
+      dueTimeInWords = "Proběhlo ${DateFormat("d.M. H:mm").format(dueTime)}";
+    } else if (difference.inMinutes == 0) {
+      dueTimeInWords = "Probíhá právě teď";
+    } else {
+      dueTimeInWords = "Proběhne za$differenceInWords (${isTomorrow ? "zítra " : ""}v ${DateFormat("H:mm").format(dueTime)})";
+    }
+
+    final creationTimeFormatted = DateFormat("d.M. H:mm").format(widget.creationTime);
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+      child: Container(
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 5.0,
+              //spreadRadius: 10.0,
+
             ),
-            const SizedBox(height: 10),
-            Text(
-              widget.textContent.toString(),
-              textAlign: TextAlign.left,
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
+          ],
+        ),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
                 Text(
-                  dueTimeFormatted,
+                  "zadáno $creationTimeFormatted",
+                  textAlign: TextAlign.left,
                   style: const TextStyle(
-                    color: Colors.grey
+                      color: Colors.grey
                   ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  widget.textContent.toString(),
                   textAlign: TextAlign.left,
                 ),
-                TextButton(
-                  child: const Text('OZNAČIT ZA DOKONČENÉ'),
-                  onPressed: () {/* ... */},
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      dueTimeInWords,
+                      style: const TextStyle(
+                        color: Colors.grey
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    TextButton(
+                      child: const Text('OZNAČIT ZA DOKONČENÉ'),
+                      onPressed: difference.isNegative || difference.inMinutes == 0 ? null : () {setState(() => timeMarkedDone = DateTime.now());},
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
